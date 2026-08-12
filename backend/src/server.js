c248b6839da31c7a4ddc86c8f0d93ba92003cf5c
Harathi2923 <harathigaddam29@gmail.com>
2026-08-12 22:54:47 +0530

    Prepare backend for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,12 @@ import createAdmin from "./utils/createAdmin.js";
 
 dotenv.config();
 
-console.log(
-  "JWT_EXPIRES_IN:",
-  process.env.JWT_EXPIRES_IN
-);
+console.log("JWT_EXPIRES_IN:", process.env.JWT_EXPIRES_IN);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  "0.0.0.0",
-  async () => {
+app.listen(PORT, async() => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 
-    console.log(
-      `🚀 Server running on port ${PORT}`
-    );
-
-    await createAdmin();
-
-  }
-);
+  await createAdmin();
+});
